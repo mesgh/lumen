202aadedaf6534b2dcffa2c31cbf01dd7e70a1fa
Mosey <moseev.es@ya.ru>
2021-06-07 20:25:38 +0300

    yeah?

--- routes/web.php
+++ routes/web.php
@@ -1,32 +1,32 @@
 <?php
 
 $router->get('/', function () use ($router) {
-    $date = new class
+  $date = new class
+  {
+    function getTime()
     {
-        function getTime()
-        {
-            return date('d/m/Y H:i');
-        }
-    };
-    return '<h1>' . $date->getTime() . '</h1>';
+      return date('d/m/Y H:i');
+    }
+  };
+  return '<h1>' . $date->getTime() . '</h1>';
 });
 
-$router->get('/?{print}[&public]}', function ($print, $public) use ($router) {
-    var_dump($print, $public);
-    if (isset($_GET['print'])) {
-        header('Content-Type: text/plain; charset=utf-8');
-        if (isset($_GET['public'])) {
-            header('Access-Control-Allow-Origin: *');
-            header('Access-Control-Allow-Methods: GET, POST, DELETE');
-        }
-        return file_get_contents(basename(__FILE__));
-    }
+$router->get('/print', function () use ($router) {
+  header('Content-Type: text/plain; charset=utf-8');
+  return file_get_contents(basename(__FILE__));
+});
+
+$router->get('/print/public', function () use ($router) {
+  header('Content-Type: text/plain; charset=utf-8');
+  header('Access-Control-Allow-Origin: *');
+  header('Access-Control-Allow-Methods: GET, POST, DELETE');
+  return file_get_contents(basename(__FILE__));
 });
 
 $router->get('/hello/{name}', function ($name) {
-    return '<h1>Hello ' . $name . '!</h1>';
+  return '<h1>Hello ' . $name . '!</h1>';
 });
 
 $router->get('/date', function () {
-    return '<h1>' . date('r') . '</h1>';
+  return '<h1>' . date('r') . '</h1>';
 });
