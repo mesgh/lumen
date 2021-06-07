82f2e6abf94f98e43fb975748807f65f7004cc0a
Mosey <moseev.es@ya.ru>
2021-06-07 20:07:25 +0300

    awdawd

--- public/index.php
+++ public/index.php
@@ -2,9 +2,4 @@
 
 $app = require __DIR__.'/../bootstrap/app.php';
 
-$app->get('/', function () {
-
-    return '<h1>'.date('r').'</h1>';
-});
-
 $app->run();
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,28 @@
 <?php
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    $date = new class
+    {
+        function getTime()
+        {
+            return date('d/m/Y H:i');
+        }
+    };
+
+    if (isset($_GET['print'])) {
+        header('Content-Type: text/plain; charset=utf-8');
+        if (isset($_GET['public'])) {
+            header('Access-Control-Allow-Origin: *');
+            header('Access-Control-Allow-Methods: GET, POST, DELETE');
+        }
+        return file_get_contents(basename(__FILE__));
+    } else {
+        return '<h1>' . $date->getTime() . '</h1>';
+    }
 });
 $router->get('/hello/{name}', function ($name) {
-    return '<h1>Hello '. $name .'!</h1>';
+    return '<h1>Hello ' . $name . '!</h1>';
 });
 $router->get('/date', function () {
-    return '<h1>'.date('r').'</h1>';
+    return '<h1>' . date('r') . '</h1>';
 });
-
